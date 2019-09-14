a448e7a24aded164fd6d14b6db9e4e5d91d9799b
Jamalansari <jamal_ansari@nobilitytechnologies.com>
2019-09-14 16:29:50 -0400

    ss

--- packages/landing-gatsby/src/containers/App/FeatureTwoSection/featureTwoSection.style.js
+++ packages/landing-gatsby/src/containers/App/FeatureTwoSection/featureTwoSection.style.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-const FeatureAppSectionWrapper = styled.section`
+const FeatureTwoSectionWrapper = styled.section`
   padding: 80px 0 100px;
   @media (max-width: 1440px) {
     padding: 40px 0 50px;
